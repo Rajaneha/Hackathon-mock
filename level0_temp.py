ae39947cbd2a8470395c0f674ea1ae80aaf1bc41
rajaneha.r09@gmail.com <PL@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 11:51:27 +0530

    algorithm

--- level0_temp.py
+++ level0_temp.py
@@ -1,4 +1,23 @@
 import json
+from sys import maxsize
+from itertools import permutations
+
+from typing import List
+import math
+
+def nearest_neighbor(neighbourhood, output):
+    unvisited = set(neighbourhood)
+    current = neighbourhood[0]
+    unvisited.remove(current)
+    tour = [current]
+    while unvisited:
+        next_city = min(unvisited, key=lambda city: output[neighbourhood.index(current)][neighbourhood.index(city)])
+        tour.append(next_city)
+        unvisited.remove(next_city)
+        current = next_city
+    return tour
+
+
 with open('Input data\level0.json','r') as f:
     data = json.load(f)
 
@@ -28,5 +47,7 @@ matrix = fromneigh_dist
 #print(matrix)
 
 output = [[v, *subl] for v, subl in zip(temprest, matrix)]
-print(output)
-
+#print(output)
+neighbourhood =["r0","n0","n1","n2","n3","n4","n5","n6","n7","n7","n8","n9","n10","n11","n12","n13","n14","n15","n16","n17","n18","n19"]
+final_result = nearest_neighbor(neighbourhood,output)
+print(final_result)
