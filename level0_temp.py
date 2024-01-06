fa590ed9d55975d18197d2babe165bf98f77b6ed
rajaneha.r09@gmail.com <PL@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 12:15:46 +0530

    nearestneighbour

--- level0_temp.py
+++ level0_temp.py
@@ -1,21 +1,18 @@
 import json
-from sys import maxsize
-from itertools import permutations
 
 from typing import List
 import math
 
-def nearest_neighbor(neighbourhood, output):
-    unvisited = set(neighbourhood)
-    current = neighbourhood[0]
-    unvisited.remove(current)
-    tour = [current]
-    while unvisited:
-        next_city = min(unvisited, key=lambda city: output[neighbourhood.index(current)][neighbourhood.index(city)])
-        tour.append(next_city)
-        unvisited.remove(next_city)
-        current = next_city
-    return tour
+def nearest_neighbour(mat):
+    global route
+    for i in range(len(mat)):
+        if(mat[i]!=0 and ('n'+str(i)) not in route):
+            m=mat[i]
+            break
+    for i in mat:
+        if (i!=0 and i<m and ('n'+str(mat.index(i))) not in route):
+            m=i
+    return mat.index(m)
 
 
 with open('Input data\level0.json','r') as f:
@@ -49,5 +46,14 @@ matrix = fromneigh_dist
 output = [[v, *subl] for v, subl in zip(temprest, matrix)]
 #print(output)
 neighbourhood =["r0","n0","n1","n2","n3","n4","n5","n6","n7","n7","n8","n9","n10","n11","n12","n13","n14","n15","n16","n17","n18","n19"]
-final_result = nearest_neighbor(neighbourhood,output)
-print(final_result)
+
+route =[]
+while(len(route)!=21):
+    i=nearest_neighbour(output)
+    route.append("n"+str(i))
+
+route.append("r0")
+p={"v0":{"Route traversed by steve":route}}
+json_object = json.dumps(p, indent=4)
+with open("level0_op.json", "w") as outfile:
+    outfile.write(json_object)
